05d4683f5a55022eeca2911132f34c3d5dc4ee72
Stuart Auchterlonie <stuarta@mythtv.org>
2016-12-21 22:54:21 +0000

    Cruft removal

--- trac/run.sh
+++ trac/run.sh
@@ -2,15 +2,6 @@
 trac-admin /opt/trac/mythtv upgrade
 trac-admin /opt/trac/mythtv wiki upgrade
 
-#tracd -p 8080 --basic-auth="*,/trac/.htpasswd,TRAC" /trac
-#tracd -p 8082 /opt/trac/mythtv
-
-#tracd -p 8082 --basic-auth="*,/opt/trac/htpasswd,TRAC" /opt/trac/mythtv
-
 rm -f /var/run/httpd/httpd.pid
 /usr/sbin/httpd -DFOREGROUND
-
-# @to-do pass args as parameter
-# echo "running: tracd -p 8080 $TRACD_ARGS /trac"
-# tracd -p 8080 $TRACD_ARGS /trac
 
