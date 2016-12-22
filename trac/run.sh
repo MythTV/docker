71be977ad782d8d7bda4cf0c83d17d4826cd4663
Stuart Auchterlonie <stuarta@mythtv.org>
2016-12-22 12:18:02 +0000

    Extra cleanup to ensure startup

--- trac/run.sh
+++ trac/run.sh
@@ -3,5 +3,7 @@ trac-admin /opt/trac/mythtv upgrade
 trac-admin /opt/trac/mythtv wiki upgrade
 
 rm -f /var/run/httpd/httpd.pid
+rm -f /var/log/httpd/*log
+rm -f /run/httpd/*
 /usr/sbin/httpd -DFOREGROUND
-
+tail -50 /var/log/httpd/error_log
