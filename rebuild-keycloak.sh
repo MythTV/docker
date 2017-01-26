c952b5159646479ed72d49eb6ab619b33af6f5c9
Stuart Auchterlonie <stuarta@mythtv.org>
2017-01-26 14:37:04 +0000

    Better separate building a new image, with rebuilding the existing container

--- rebuild-keycloak.sh
+++ rebuild-keycloak.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker rm keycloak
 docker rmi mythtv/keycloak
 docker build --tag mythtv/keycloak /root/docker/keycloak
+
+echo "Please tag the mythtv/keycloak image with the current keycloak version"
