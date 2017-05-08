decddf79d2241d829bed0ae2f98dcf2c91fa0ded
Stuart Auchterlonie <stuarta@squashedfrog.net>
2017-05-08 13:09:25 +0100

    Remove httpd_lua_shm temp files on startup

--- trac/run.sh
+++ trac/run.sh
@@ -5,5 +5,6 @@ trac-admin /opt/trac/mythtv wiki upgrade
 rm -f /var/run/httpd/httpd.pid
 rm -f /var/log/httpd/*log
 rm -f /run/httpd/*
+rm -f /tmp/httpd_lua_shm*
 /usr/sbin/httpd -DFOREGROUND
 tail -50 /var/log/httpd/error_log
