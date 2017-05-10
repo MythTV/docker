426cc331435e416e1da390388d3d4a8e0295e3ba
Stuart Auchterlonie <stuarta@mythtv.org>
2017-05-10 21:09:22 +0100

    Use the shared volume selinux tag for the git repos

--- rebuild-cgit.sh
+++ rebuild-cgit.sh
@@ -2,4 +2,4 @@
 docker rm cgit
 docker rmi mythtv/cgit
 docker build --tag mythtv/cgit /root/docker/cgit
-docker run -d -p 8084:80 --name cgit -v /opt/gitolite:/opt/gitolite:Z --log-driver=journald mythtv/cgit
+docker run -d -p 8084:80 --name cgit -v /opt/gitolite:/opt/gitolite:z --log-driver=journald mythtv/cgit
--- rebuild-trac.sh
+++ rebuild-trac.sh
@@ -11,4 +11,4 @@ pushd trac
 python ./generate-auth-config.py > sso.credentials
 popd
 docker build --tag mythtv/trac /root/docker/trac
-docker run -d -p 8082:80 --name trac -v /opt/trac:/opt/trac:Z -v /opt/gitolite:/opt/gitolite:Z --log-driver=journald mythtv/trac
+docker run -d -p 8082:80 --name trac -v /opt/trac:/opt/trac:Z -v /opt/gitolite:/opt/gitolite:z --log-driver=journald mythtv/trac
