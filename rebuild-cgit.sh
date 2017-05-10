4aa6b88427d39f2086ae9b1d1316da914092ed8d
Stuart Auchterlonie <stuarta@mythtv.org>
2017-05-10 12:08:42 +0100

    Initial cgit Docker setup

---
+++ rebuild-cgit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker rm cgit
+docker rmi mythtv/cgit
+docker build --tag mythtv/cgit /root/docker/cgit
+docker run -d -p 8084:80 --name cgit -v /opt/gitolite:/opt/gitolite:Z --log-driver=journald mythtv/cgit
