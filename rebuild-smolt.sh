4409c70a9ee59c660f044403392d140966d6789c
Stuart Auchterlonie <stuarta@mythtv.org>
2019-03-18 12:10:29 +0000

    Separate build and deploy of smolt container

--- rebuild-smolt.sh
+++ rebuild-smolt.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-docker rm smolt
 docker rmi mythtv/smolt
 docker build --tag mythtv/smolt /root/docker/smolt
-docker run -d -p 8086:8086 --name smolt -v /opt/smoon_server:/opt/smoon_server:Z --log-driver=journald mythtv/smolt
